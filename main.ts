11316a85c1fa7fbdc8f66ed449562be7201f1a53
DziveQ <56726128+DziveQ@users.noreply.github.com>
2020-11-24 13:12:12 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-    basic.showString("DINGS")
+input.onButtonPressed(Button.A, function () {
+    for(let i = 0; i <= 3; i++) {
+       basic.showString("DINGS")
+       basic.showIcon(IconNames.Heart)
+       basic.pause(100)
+    }
 })
